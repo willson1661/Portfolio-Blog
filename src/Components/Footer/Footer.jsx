0610c6d6ce6bbed6a35e885167b98bb6cb5a1c12
willson <93517421+willson1661@users.noreply.github.com>
2023-12-02 16:12:04 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = ()=>{
             <div className="Footer_main">
 <div className="reg1">I'd love to hear from you.</div>
 <div className="contact1">
-<h3><AiOutlineMail /> officialwillson05@gmail.com</h3>
-<h3><AiOutlineMobile /> 9052830829</h3>
-<h3><AiOutlineHome /> 39-2-26, Badangpet, Hyderabad, Telangana, India.</h3>
-<a href={Mypdf} download="/Willsonraj_manda.pdf"><button className="resumebtn1">Download Resume</button></a>
+<h3><AiOutlineMail /> </h3>
+<h3><AiOutlineMobile /> </h3>
+<h3><AiOutlineHome /> </h3>
+<a href={Mypdf} ><button className="resumebtn1">Download Resume</button></a>
 
 
 </div>
